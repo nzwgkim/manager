4d011b713cbd766ab8dbe47c4bbc0a856a816be2
Woody <nz.wgkim@gmail.com>
2023-02-25 16:46:42 +1300

    GridView, ReorderableListView, ListView

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,23 @@ class SimpleProvider extends ChangeNotifier {
   }
 }
 
+final colors = [
+  Colors.red,
+  Colors.orange,
+  Colors.yellow,
+  Colors.green,
+  Colors.blue,
+  Colors.indigo,
+  Colors.purple
+];
+
+Widget renderContainer({required Color color}) {
+  return Container(
+    height: 300,
+    color: color,
+  );
+}
+
 class Home extends StatelessWidget {
   // var num = 1.obs;
   var control = Get.put(SimpleGeX());
@@ -64,55 +81,194 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Center(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Obx(() => Text(
+                '${control.count}',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              )),
+          ElevatedButton(
+              onPressed: () {
+                control.increase();
+              },
+              child: const Text(
+                'Increase',
+                style: TextStyle(fontSize: 20, color: Colors.yellow),
+              )),
+          ElevatedButton(
+              onPressed: () {
+                Get.to(() => First());
+              },
+              child: const Text(
+                'First',
+                style: TextStyle(fontSize: 20),
+              )),
+          ElevatedButton(
+              onPressed: () {
+                Get.toNamed('/Second/Hi');
+              },
+              child: const Text(
+                'Second',
+                style: TextStyle(fontSize: 20),
+              )),
+          ElevatedButton(
+              onPressed: () {
+                Get.toNamed('/Third?id=woody');
+              },
+              child: const Text(
+                'Third',
+                style: TextStyle(fontSize: 20),
+              )),
+          ElevatedButton(
+              onPressed: () {
+                Get.to(() => const Rainbow());
+              },
+              child: const Text(
+                'Rainbow',
+                style: TextStyle(fontSize: 20),
+              )),
+          ElevatedButton(
+              onPressed: () {
+                Get.to(() => const RainbowListView(title: 'RainbowListView'));
+              },
+              child: const Text(
+                'RainbowListView',
+                style: TextStyle(fontSize: 20),
+              )),
+          ElevatedButton(
+              onPressed: () {
+                Get.to(() => const RainbowReorderableListView(
+                    title: 'RainbowReorderableListView'));
+              },
+              child: const Text(
+                'RainbowReorderableListView',
+                style: TextStyle(fontSize: 20),
+              )),
+          ElevatedButton(
+              onPressed: () {
+                Get.to(() => const RainbowGridView(title: 'RainbowGridView'));
+              },
+              child: const Text(
+                'RainbowGridView',
+                style: TextStyle(fontSize: 20),
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class RainbowGridView extends StatelessWidget {
+  final String title;
+  const RainbowGridView({required this.title, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 100),
+        itemBuilder: (context, index) =>
+            renderContainer(color: colors[index % 7]),
+        itemCount: 300,
+      ),
+      // GridView.count(
+      //   crossAxisCount: 2,
+      //   children: colors.map((e) => renderContainer(color: e)).toList(),
+      // ),
+    );
+  }
+}
+
+class RainbowReorderableListView extends StatefulWidget {
+  final String title;
+  const RainbowReorderableListView({required this.title, super.key});
+
+  @override
+  State<RainbowReorderableListView> createState() =>
+      _RainbowReorderableListViewState();
+}
+
+class _RainbowReorderableListViewState
+    extends State<RainbowReorderableListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ReorderableListView.builder(
+          itemBuilder: (context, index) =>
+              reoderableRenderContainer(color: colors[index], index: index),
+          itemCount: colors.length,
+          onReorder: (oldIndex, newIndex) {
+            if (oldIndex < newIndex) {
+              newIndex--;
+            }
+            setState(() {
+              final item = colors.removeAt(oldIndex);
+              colors.insert(newIndex, item);
+            });
+          },
+
+          // children: colors.map((e) => renderContainer(color: e)).toList()),
+        ));
+  }
+
+  Widget reoderableRenderContainer({required int index, required Color color}) {
+    return Container(
+      key: Key(index.toString()),
+      height: 100,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: const TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
+
+class RainbowListView extends StatelessWidget {
+  final String title;
+  const RainbowListView({required this.title, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(title)),
+        body: ListView.separated(
+          itemBuilder: (context, index) =>
+              renderContainer(color: colors[index]),
+          itemCount: colors.length,
+          separatorBuilder: (context, index) => const SizedBox(
+            height: 5,
+          ),
+        ));
+  }
+}
+
+class Rainbow extends StatelessWidget {
+  const Rainbow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rainbow'),
+      ),
+      body: SingleChildScrollView(
         child: Column(
-          children: [
-            Obx(() => Text(
-                  '${control.count}',
-                  style: const TextStyle(fontSize: 20),
-                )),
-            ElevatedButton(
-                onPressed: () {
-                  control.increase();
-                },
-                child: const Text(
-                  'Increase',
-                  style: TextStyle(fontSize: 20),
-                )),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Get.to(() => First());
-                },
-                child: const Text(
-                  'First',
-                  style: TextStyle(fontSize: 20),
-                )),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed('/Second/Hi');
-                },
-                child: const Text(
-                  'Second',
-                  style: TextStyle(fontSize: 20),
-                )),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Get.toNamed('/Third?id=woody');
-                },
-                child: const Text(
-                  'Third',
-                  style: TextStyle(fontSize: 20),
-                )),
-          ],
+          children: colors.map((e) => renderContainer(color: e)).toList(),
         ),
       ),
     );
