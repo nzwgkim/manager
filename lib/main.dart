16095598e5b0d4be2334238b05086b6d3c653bf5
Woody <nz.wgkim@gmail.com>
2023-02-26 05:35:43 +1300

    SliverList in CustomScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:manager/scrollable.dart';
 import 'package:manager/state_managers.dart';
 import 'package:provider/provider.dart';
 
@@ -51,28 +52,6 @@ class SimpleProvider extends ChangeNotifier {
     _count++;
     notifyListeners();
   }
-}
-
-final colors = [
-  Colors.red,
-  Colors.orange,
-  Colors.yellow,
-  Colors.green,
-  Colors.blue,
-  Colors.indigo,
-  Colors.purple
-];
-
-Widget renderContainer({required Color color, required int index}) {
-  return Container(
-    height: 300,
-    color: color,
-    alignment: Alignment.center,
-    child: Text(
-      index.toString(),
-      style: const TextStyle(fontSize: 30),
-    ),
-  );
 }
 
 class Home extends StatelessWidget {
@@ -163,121 +142,16 @@ class Home extends StatelessWidget {
                 'RainbowGridView',
                 style: TextStyle(fontSize: 20),
               )),
+          ElevatedButton(
+              onPressed: () {
+                Get.to(() =>
+                    RainbowCustomScrollView(title: 'RainbowCustomScrollView'));
+              },
+              child: const Text(
+                'RainbowCustomScrollView',
+                style: TextStyle(fontSize: 20),
+              )),
         ],
-      ),
-    );
-  }
-}
-
-class RainbowGridView extends StatelessWidget {
-  final String title;
-  const RainbowGridView({required this.title, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 100),
-        itemBuilder: (context, index) =>
-            renderContainer(color: colors[index % 7], index: index),
-        itemCount: 300,
-      ),
-      // GridView.count(
-      //   crossAxisCount: 2,
-      //   children: colors.map((e) => renderContainer(color: e)).toList(),
-      // ),
-    );
-  }
-}
-
-class RainbowReorderableListView extends StatefulWidget {
-  final String title;
-  const RainbowReorderableListView({required this.title, super.key});
-
-  @override
-  State<RainbowReorderableListView> createState() =>
-      _RainbowReorderableListViewState();
-}
-
-class _RainbowReorderableListViewState
-    extends State<RainbowReorderableListView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ReorderableListView.builder(
-          itemBuilder: (context, index) =>
-              reoderableRenderContainer(color: colors[index], index: index),
-          itemCount: colors.length,
-          onReorder: (oldIndex, newIndex) {
-            if (oldIndex < newIndex) {
-              newIndex--;
-            }
-            setState(() {
-              final item = colors.removeAt(oldIndex);
-              colors.insert(newIndex, item);
-            });
-          },
-
-          // children: colors.map((e) => renderContainer(color: e)).toList()),
-        ));
-  }
-
-  Widget reoderableRenderContainer({required int index, required Color color}) {
-    return Container(
-      key: Key(index.toString()),
-      height: 100,
-      color: color,
-      child: Center(
-        child: Text(
-          index.toString(),
-          style: const TextStyle(fontSize: 30),
-        ),
-      ),
-    );
-  }
-}
-
-class RainbowListView extends StatelessWidget {
-  final String title;
-  const RainbowListView({required this.title, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text(title)),
-        body: ListView.separated(
-          itemBuilder: (context, index) =>
-              renderContainer(color: colors[index], index: index),
-          itemCount: colors.length,
-          separatorBuilder: (context, index) => const SizedBox(
-            height: 5,
-          ),
-        ));
-  }
-}
-
-class Rainbow extends StatelessWidget {
-  const Rainbow({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    int i = 0;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Rainbow'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children:
-              colors.map((e) => renderContainer(color: e, index: i++)).toList(),
-        ),
       ),
     );
   }
---
+++ lib/scrollable.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+final colors = [
+  Colors.red,
+  Colors.orange,
+  Colors.yellow,
+  Colors.green,
+  Colors.blue,
+  Colors.indigo,
+  Colors.purple
+];
+
+Widget renderContainer({required Color color, required int index}) {
+  print('render# $index');
+  return Container(
+    height: 200,
+    color: color,
+    alignment: Alignment.center,
+    child: Text(
+      index.toString(),
+      style: const TextStyle(fontSize: 30),
+    ),
+  );
+}
+
+class RainbowGridView extends StatelessWidget {
+  final String title;
+  const RainbowGridView({required this.title, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 100),
+        itemBuilder: (context, index) =>
+            renderContainer(color: colors[index % 7], index: index),
+        itemCount: 300,
+      ),
+      // GridView.count(
+      //   crossAxisCount: 2,
+      //   children: colors.map((e) => renderContainer(color: e)).toList(),
+      // ),
+    );
+  }
+}
+
+class RainbowReorderableListView extends StatefulWidget {
+  final String title;
+  const RainbowReorderableListView({required this.title, super.key});
+
+  @override
+  State<RainbowReorderableListView> createState() =>
+      _RainbowReorderableListViewState();
+}
+
+class _RainbowReorderableListViewState
+    extends State<RainbowReorderableListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ReorderableListView.builder(
+          itemBuilder: (context, index) =>
+              reoderableRenderContainer(color: colors[index], index: index),
+          itemCount: colors.length,
+          onReorder: (oldIndex, newIndex) {
+            if (oldIndex < newIndex) {
+              newIndex--;
+            }
+            setState(() {
+              final item = colors.removeAt(oldIndex);
+              colors.insert(newIndex, item);
+            });
+          },
+
+          // children: colors.map((e) => renderContainer(color: e)).toList()),
+        ));
+  }
+
+  Widget reoderableRenderContainer({required int index, required Color color}) {
+    return Container(
+      key: Key(index.toString()),
+      height: 100,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: const TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
+
+class RainbowListView extends StatelessWidget {
+  final String title;
+  const RainbowListView({required this.title, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(title)),
+        body: ListView.separated(
+          itemBuilder: (context, index) =>
+              renderContainer(color: colors[index], index: index),
+          itemCount: colors.length,
+          separatorBuilder: (context, index) => const SizedBox(
+            height: 5,
+          ),
+        ));
+  }
+}
+
+class Rainbow extends StatelessWidget {
+  const Rainbow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int i = 0;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rainbow'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children:
+              colors.map((e) => renderContainer(color: e, index: i++)).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class RainbowCustomScrollView extends StatelessWidget {
+  List<int> count = List.generate(10, (index) => index);
+  final String title;
+  RainbowCustomScrollView({required this.title, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(title),
+          ),
+          renderSliverList(),
+          renderSliverListBuild(),
+        ],
+      ),
+    );
+  }
+
+  SliverList renderSliverList() {
+    return SliverList(
+        delegate: SliverChildListDelegate(count
+            .map((e) =>
+                renderContainer(color: colors[e % colors.length], index: e))
+            .toList()));
+  }
+
+  SliverList renderSliverListBuild() {
+    return SliverList(
+        delegate: SliverChildBuilderDelegate(
+            (context, index) => renderContainer(
+                color: colors[index % colors.length], index: index),
+            childCount: 100));
+  }
+}
